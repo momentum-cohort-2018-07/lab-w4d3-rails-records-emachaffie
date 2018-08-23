531a0283f6a0df1632afe10fc47a8066d4a9b94f
emachaffie <emachaffie@gmail.com>
2018-08-23 10:02:54 -0400

    Finished adding all CRUD actions

--- dog_breeds/app/controllers/dogs_controller.rb
+++ dog_breeds/app/controllers/dogs_controller.rb
@@ -5,16 +5,40 @@ class DogsController < ApplicationController
   end
 
   def new 
+    @dog = Dog.new
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
   end
 
   def create
     @dog = Dog.new(dog_params)
-    @dog.save
-    redirect_to @dog
+    if @dog.save
+      redirect_to @dog
+    else
+      render 'new'
+    end
   end
  
   def show
     @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      redirect_to @dog
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+   
+    redirect_to dogs_path
   end
 
   private
